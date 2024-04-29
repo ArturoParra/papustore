681e8893853a37275fc557c55d5fbbce855c58ed
Arroyo Mendoza Jesús David <84303540+JisusArroyoDev@users.noreply.github.com>
2024-04-29 10:34:21 -0600

    Mis cambios locales

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        hex_0554F2 : '#0554F2',
+        hex_076DF2 : '#076DF2',
+        hex_3889F2 : '#3889F2',
+        hex_F2F2F2 : '#F2F2F2',
+        hex_0D0D0D : '#0D0D0D',
+      },
+    },
   },
   plugins: [],
 }
