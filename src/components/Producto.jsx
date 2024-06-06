7ebb3845e265545c184dcfe2a0dd63123dfb6c17
Artur <arturogparrarivas@gmail.com>
2024-06-06 16:22:34 -0600

    Consulta a BD y desestructuracion en componente VistaProducto

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -24,7 +24,7 @@ export const Producto = ({producto}) => {
       <div className="border border-slate-200 rounded-md p-3 shadow-md max-h-fit content-end justify-items-stretch">
           {/*TODO: centrar la imagen*/}
         <Link to="/producto" state={productoActualizado}>
-          <img className="object-cover mx-auto my-auto" src={thumbnail} alt="imagen guitarra" />
+          <img className="object-cover mx-auto my-auto" src={thumbnail} alt="imagen producto" />
         </Link>
 
         <div className="pt-2">
