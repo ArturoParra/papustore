a230a18d5a6a6455cc52fedd1471f7f85205c8af
Artur <arturogparrarivas@gmail.com>
2024-06-12 19:41:19 -0600

    Conflictos en IndexPage

--- src/views/IndexPage.jsx
+++ src/views/IndexPage.jsx
@@ -3,7 +3,6 @@ import { Header } from "../components/Header"
 import { Ofertas } from "../components/Ofertas"
 import { FormularioInicio } from "./FormularioInicio"
 import { Shop } from "./Shop"
-import { FormularioInicio } from './FormularioInicio'
 import { AdministratorMode } from "./AdministratorMode"
 import { FormularioAdministrator } from "./FormularioAdministrator"
 
